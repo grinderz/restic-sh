f8bfa838794e165e630f9112e62d57dfcc6a6285
grinderz <grinderz@protonmail.com>
2019-06-07 16:57:38 +0300

    clean-all

--- restic.sh
+++ restic.sh
@@ -5,40 +5,47 @@ set -euo pipefail
 source ~/restic.env
 
 KEEP_POLICY=(
-	--keep-last=2
-	--keep-daily=7
-	--keep-weekly=8
-	--keep-monthly=12
-	--keep-yearly=10
+    --keep-last=2
+    --keep-daily=7
+    --keep-weekly=8
+    --keep-monthly=12
+    --keep-yearly=10
 )
 
 function backup {
-	~/bin/restic backup \
+    ~/bin/restic backup \
 		--files-from=${RESTIC_FILESFROM} \
 		--tag=cron \
 		--cache-dir=${RESTIC_CACHEDIR} \
 		--exclude-caches
-	~/bin/restic forget \
+    ~/bin/restic forget \
 		--tag=cron \
 		--cache-dir=${RESTIC_CACHEDIR} \
 		--prune ${KEEP_POLICY[@]}
 }
 
 function check {
-	~/bin/restic check \
-		--read-data-subset=$(($RANDOM % 4 + 1))/4
+    ~/bin/restic check \
+    	--read-data-subset=$(($RANDOM % 4 + 1))/4
+}
+
+function check_all {
+    ~/bin/restic check
 }
 
 case "$1" in
-	--backup)
-		backup
-		;;
-	--check)
-		check
-		;;
-	*)
-		~/bin/restic "$@" \
-			--cache-dir=${RESTIC_CACHEDIR}
-		;;
+    --backup)
+        backup
+    ;;
+    --check)
+        check
+    ;;
+    --check-all)
+        check_all
+    ;;
+    *)
+        ~/bin/restic "$@" \
+        	--cache-dir=${RESTIC_CACHEDIR}
+    ;;
 esac
 
